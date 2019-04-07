5570ce60c86c94f77ecc587cfdc4d6d5de981179
webistomin <webistomin@gmail.com>
2019-04-07 16:05:49 +0300

    add sass var loader

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
 <template lang="pug">
   #app
 </template>
+
+<style lang="sass">
+  body
+    background-color: $color-green
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  css: {
+    loaderOptions:
+      {
+        sass:
+          {
+            data: '@import "~@/assets/sass/main.sass";',
+          },
+      },
+  },
   pwa: {
     themeColor: '#7540EE',
     msTileColor: '#7540EE',
