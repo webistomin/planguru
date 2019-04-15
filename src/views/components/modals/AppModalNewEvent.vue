e7b25d8b4867a9aa1fdd2c640a89b23b848d6cb5
webistomin <webistomin@gmail.com>
2019-04-15 19:56:47 +0300

    start new event mnodal validation

--- src/views/components/modals/AppModalNewEvent.vue
+++ src/views/components/modals/AppModalNewEvent.vue
@@ -9,30 +9,73 @@
     .modal__inner
       button.modal__close(type="button")
       form.modal__form
-        input.modal__input(type="text" placeholder="Name your meeting" autofocus)
+        input.modal__input(
+          type="text"
+          placeholder="Name your meeting"
+          v-model="meetingName"
+          autofocus)
         fieldset.modal__group
-          legend.modal__legend Invite people
+          .modal__holder
+            legend.modal__legend Invite people
+            p.modal__error.error
           button.modal__btn(type="button")
         fieldset.modal__group
-          legend.modal__legend Choose date
+          .modal__holder
+            legend.modal__legend Choose date
+            p.modal__error.error
           button.modal__date(type="button") Today, 08:00 — 09:00 AM
         fieldset.modal__group
           legend.modal__legend Choose meeting type
           .modal__wrap
             .modal__box
-              input.modal__radio(type="radio" name="meeting-type" id="type-project-meeting")
+              input.modal__radio(
+                type="radio"
+                name="meeting-type"
+                id="type-project-meeting"
+                value="project-meeting"
+                v-model="meetingType"
+                :class="{'modal__radio_disabled': meetingType && meetingType !== 'project-meeting'}"
+                )
               label.modal__label.chip.chip_green(for="type-project-meeting") Project meeting
             .modal__box
-              input.modal__radio(type="radio" name="meeting-type" id="type-meeting")
+              input.modal__radio(
+                type="radio"
+                name="meeting-type"
+                id="type-meeting"
+                value="meeting"
+                v-model="meetingType"
+                :class="{'modal__radio_disabled': meetingType && meetingType !== 'meeting'}"
+                )
               label.modal__label.chip.chip_blue(for="type-meeting") Meeting
             .modal__box
-              input.modal__radio(type="radio" name="meeting-type" id="type-webinar")
+              input.modal__radio(
+                type="radio"
+                name="meeting-type"
+                id="type-webinar"
+                value="webinar"
+                v-model="meetingType"
+                :class="{'modal__radio_disabled': meetingType && meetingType !== 'webinar'}"
+                )
               label.modal__label.chip.chip_orange(for="type-webinar") Webinar
             .modal__box
-              input.modal__radio(type="radio" name="meeting-type" id="type-status")
+              input.modal__radio(
+                type="radio"
+                name="meeting-type"
+                id="type-status"
+                value="status"
+                v-model="meetingType"
+                :class="{'modal__radio_disabled': meetingType && meetingType !== 'status'}"
+                )
               label.modal__label.chip.chip_purple(for="type-status") Status
             .modal__box
-              input.modal__radio(type="radio" name="meeting-type" id="type-other")
+              input.modal__radio(
+                type="radio"
+                name="meeting-type"
+                id="type-other"
+                value="other"
+                v-model="meetingType"
+                :class="{'modal__radio_disabled': meetingType && meetingType !== 'other'}"
+                )
               label.modal__label.chip.chip_lightblue(for="type-other") Other
         fieldset.modal__group
           legend.modal__legend White short description
@@ -43,6 +86,12 @@
 <script>
 export default {
   name: 'AppModalNewEvent',
+  data() {
+    return {
+      meetingName: '',
+      meetingType: '',
+    };
+  },
 };
 </script>
 
