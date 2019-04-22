5bff43266b34f1e41587db888d467feea397eeae
webistomin <webistomin@gmail.com>
2019-04-22 18:34:59 +0300

    still working on calendar

--- src/views/components/TheCalendar.vue
+++ src/views/components/TheCalendar.vue
@@ -1,72 +1,69 @@
 <template lang="pug">
   section.calendar(@scroll="setRoomSize")
     ul.calendar__times
-      li.calendar__time(v-for="i of 20") 09:00
+      li.calendar__time(v-for="i of workingTimes") {{i}}
     ul.calendar__events
-      li.calendar__current
-      li.calendar__row
-        div.calendar__cells
-          div.calendar__room
-            h2.calendar__roomname Leeroy Jenkins
-            p.calendar__seats 4 – 6 peoples
-          button.calendar__event(v-for="i of 20")
-      li.calendar__row
-        div.calendar__cells
-          div.calendar__room
-            h2.calendar__roomname Leeroy Jenkins
-            p.calendar__seats 4 – 6 peoples
-          button.calendar__event(v-for="i of 20")
-      li.calendar__row
-        div.calendar__cells
-          div.calendar__room
-            h2.calendar__roomname Leeroy Jenkins
-            p.calendar__seats 4 – 6 peoples
-          button.calendar__event(v-for="i of 20")
-      li.calendar__row
-        div.calendar__cells
-          div.calendar__room
-            h2.calendar__roomname Icanhascheezburger
-            p.calendar__seats 4 – 6 peoples
-          button.calendar__event(v-for="i of 20")
-      li.calendar__row
-        div.calendar__cells
-          div.calendar__room
-            h2.calendar__roomname Rick Roll
-            p.calendar__seats 4 – 6 peoples
-          button.calendar__event(v-for="i of 20")
-      li.calendar__row
-        div.calendar__cells
-          div.calendar__room
-            h2.calendar__roomname Peanut Butter Jelly
-            p.calendar__seats 4 – 6 peoples
-          button.calendar__event(v-for="i of 20")
-      li.calendar__row
-        div.calendar__cells
-          div.calendar__room
-            h2.calendar__roomname Peanut Butter Jelly
-            p.calendar__seats 4 – 6 peoples
-          button.calendar__event(v-for="i of 20")
-      li.calendar__row
-        div.calendar__cells
-          div.calendar__room
-            h2.calendar__roomname Peanut Butter Jelly
-            p.calendar__seats 4 – 6 peoples
-          button.calendar__event(v-for="i of 20")
-      li.calendar__row
-        div.calendar__cells
-          div.calendar__room
-            h2.calendar__roomname Peanut Butter Jelly
-            p.calendar__seats 4 – 6 peoples
-          button.calendar__event(v-for="i of 20")
-
-    //.calendar__current
-    //ul.calendar__events
-      li.calendar__event.calendar__event_meeting
-        h3.calendar__title Project daily stand-up
-        .calendar__date
-          time.calendar__datetime 10:00 AM
-          span.calendar__dash &nbsp;–&nbsp;
-          time.calendar__datetime 10:30 AM
+      li.calendar__current(ref="currentTimeLine")
+      li.calendar__floor
+        h1.calendar__stage First floor
+        div.calendar__row
+          div.calendar__cells
+            div.calendar__room
+              h2.calendar__roomname Leeroy Jenkins
+              p.calendar__seats 4 – 6 peoples
+            button.calendar__event(v-for="i of workingTimes")
+            button.calendar__event.calendar__event_fill
+              .calendar__overlay
+        div.calendar__row
+          div.calendar__cells
+            div.calendar__room
+              h2.calendar__roomname Leeroy Jenkins
+              p.calendar__seats 4 – 6 peoples
+            button.calendar__event(v-for="i of workingTimes")
+        div.calendar__row
+          div.calendar__cells
+            div.calendar__room
+              h2.calendar__roomname Leeroy Jenkins
+              p.calendar__seats 4 – 6 peoples
+            button.calendar__event(v-for="i of workingTimes")
+      li.calendar__floor
+        h1.calendar__stage Second floor
+        div.calendar__row
+          div.calendar__cells
+            div.calendar__room
+              h2.calendar__roomname Icanhascheezburger
+              p.calendar__seats 4 – 6 peoples
+            button.calendar__event(v-for="i of workingTimes")
+        div.calendar__row
+          div.calendar__cells
+            div.calendar__room
+              h2.calendar__roomname Rick Roll
+              p.calendar__seats 4 – 6 peoples
+            button.calendar__event(v-for="i of workingTimes")
+        div.calendar__row
+          div.calendar__cells
+            div.calendar__room
+              h2.calendar__roomname Peanut Butter Jelly
+              p.calendar__seats 4 – 6 peoples
+            button.calendar__event(v-for="i of workingTimes")
+        div.calendar__row
+          div.calendar__cells
+            div.calendar__room
+              h2.calendar__roomname Peanut Butter Jelly
+              p.calendar__seats 4 – 6 peoples
+            button.calendar__event(v-for="i of workingTimes")
+        div.calendar__row
+          div.calendar__cells
+            div.calendar__room
+              h2.calendar__roomname Peanut Butter Jelly
+              p.calendar__seats 4 – 6 peoples
+            button.calendar__event(v-for="i of workingTimes")
+        div.calendar__row
+          div.calendar__cells
+            div.calendar__room
+              h2.calendar__roomname Peanut Butter Jelly
+              p.calendar__seats 4 – 6 peoples
+            button.calendar__event(v-for="i of workingTimes")
     AppAddEventButton
     AppModalNewEvent
 </template>
@@ -78,6 +75,28 @@ import AppModalNewEvent from '@/views/components/modals/AppModalNewEvent.vue';
 export default {
   name: 'TheCalendar',
   components: { AppModalNewEvent, AppAddEventButton },
+  data() {
+    return {
+      workingTimes: ['09:00', '09:30', '10:00',
+        '10:30', '11:00', '11:30',
+        '12:00', '12:30', '01:00',
+        '01:30', '02:00', '02:30',
+        '03:00', '03:30', '04:00',
+        '04:30', '05:00', '05:30',
+        '06:00', '06:30', '07:00',
+        '07:30', '08:00', '09:30',
+        '10:00'],
+    };
+  },
+  mounted() {
+    const today = new Date();
+    const hour = today.getHours();
+    const minutes = today.getMinutes();
+
+    const currentHourDifference = hour;
+    console.log(currentHourDifference);
+    this.$refs.currentTimeLine.style.transform = `translateX(${currentHourDifference * 60 + 152 + minutes}px)`;
+  },
   methods: {
     setRoomSize(e) {
       const rows = document.querySelectorAll('.calendar__row');
